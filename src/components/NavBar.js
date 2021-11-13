3f1186d6959e2c8aa017af662433f08d5bbe2163
jmzacarias <juanzacarias@gmail.com>
2021-11-13 18:06:25 -0300

    proyecto React.JS

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+<header>
+    <nav className= "menu">
+            <ul>
+                <li>Home</li>
+                <li>Itinerario</li>
+                <li>Galeria</li>
+                <li>Compras</li>
+            </ul>
+    </nav>
+</header>
+)
+};
+
+export default NavBar;
